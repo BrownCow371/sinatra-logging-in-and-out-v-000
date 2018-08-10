19a80e8e00a0765309876f4e87db158f9aaa8add
Jamie Brown <jmbrowncow@gmail.com>
2018-08-10 15:20:20 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,5 @@
 class Helpers
 
-<<<<<<< HEAD
   def self.current_user(session_hash)
     User.find(session_hash[:user_id])
 
@@ -8,11 +7,9 @@ class Helpers
 
   def self.is_logged_in?(session_hash)
     !!session_hash[:user_id]
-=======
   def current_user
   end
 
   def is_logged_in?
->>>>>>> 78cded4c4761e737d6e296741d2c36f753ef909b
   end
 end
