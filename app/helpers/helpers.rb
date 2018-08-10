b687c522fcb7af5f9b74959f6a3183e463ac3b9f
Jamie Brown <jmbrowncow@gmail.com>
2018-08-10 15:18:17 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,5 +1,6 @@
 class Helpers
 
+<<<<<<< HEAD
   def self.current_user(session_hash)
     User.find(session_hash[:user_id])
 
@@ -7,5 +8,11 @@ class Helpers
 
   def self.is_logged_in?(session_hash)
     !!session_hash[:user_id]
+=======
+  def current_user
+  end
+
+  def is_logged_in?
+>>>>>>> 78cded4c4761e737d6e296741d2c36f753ef909b
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 class User <ActiveRecord::Base
+=======
+class User
+>>>>>>> 78cded4c4761e737d6e296741d2c36f753ef909b
 end
